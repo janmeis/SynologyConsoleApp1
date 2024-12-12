eb41ff15d45c18e49daea7a7b83d9a5b2fc38c12
Jan Meissner <janmeis@yahoo.co.uk>
2024-12-12 14:03:02 +0100

    convert to internal class Program

--- Program.cs
+++ Program.cs
@@ -2,59 +2,112 @@
 using System.Text.Json;
 using System.Web;
 
-var baseUrl = "http://synologyds920:5000/webapi/entry.cgi";
-var queryString = HttpUtility.ParseQueryString(string.Empty);
-JsonSerializerOptions jsonOptions = new()
+internal class Program
 {
-    PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
-};
-var synoToken = string.Empty;
-var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-var account = config.GetSection("synoApiAuth")["account"]; ;
-var passwd = config.GetSection("synoApiAuth")["passwd"]; ;
-
-using (HttpClient client = new())
-{
-    queryString["api"] = "SYNO.API.Auth";
-    queryString["version"] = "6";
-    queryString["method"] = "login";
-    queryString["account"] = account;
-    queryString["passwd"] = passwd;
-    queryString["enable_syno_token"] = "yes";
-    var url = string.Join("?", baseUrl, queryString.ToString());
-    var response = await client.GetAsync(url);
-    if (response.IsSuccessStatusCode)
+    private static string baseUrl = "http://synologyds920:5000/webapi/entry.cgi";
+    private static readonly JsonSerializerOptions jsonOptions = new()
     {
-        string result = await response.Content.ReadAsStringAsync();
-        var authResponse = JsonSerializer.Deserialize<ApiAuthResponse>(result, jsonOptions);
-        if (authResponse?.Success == true)
-            synoToken = authResponse.Data.Synotoken;
-    }
+        PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+    };
 
-    queryString.Clear();
-    queryString["api"] = "SYNO.FileStation.List";
-    queryString["version"] = "2";
-    queryString["method"] = "list";
-    queryString["folder_path"] = "/music";
-    queryString["sort_by"] = "crtime";
-    queryString["sort_direction"] = "desc";
-    queryString["filetype"] = "dir";
-    queryString["additional"] = "[\"real_path\",\"time\"]";
-    queryString["SynoToken"] = synoToken;
-    url = string.Join("?", baseUrl, queryString.ToString());
-    response = await client.GetAsync(url);
-    if (response.IsSuccessStatusCode)
+    private static async Task Main(string[] args)
     {
-        var result = await response.Content.ReadAsStringAsync();
-        var listResponse = JsonSerializer.Deserialize<FileStationListResponse>(result, jsonOptions);
-        if (listResponse?.Success == true && listResponse.Data.Files != null)
+
+        string synoToken = string.Empty;
+        const int maxDirCount = 25;
+
+        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+        var section = config.GetSection("synoApiAuth");
+        var account = section["account"]; ;
+        var passwd = section["passwd"];
+
+        using (HttpClient client = new())
         {
-            foreach (var file in listResponse.Data.Files)
-                Console.WriteLine(file.Name);
+            var authData = await GetAuthAsync(account, passwd, client);
+            synoToken = authData.Data.Synotoken ?? string.Empty;
 
-            Console.WriteLine("Total files: " + listResponse.Data.Files.Count);
+            var listResponse = await GetFileListAsync(client, synoToken);
+            if (listResponse?.Success == true && listResponse?.Data?.Files != null)
+            {
+                for (int i = 0; i < Math.Min(listResponse.Data.Files.Count, maxDirCount); i++)
+                {
+                    var file = listResponse.Data?.Files[i];
+                    var dateTime = UnixTimeStampToDateTime(file?.Additional?.Time?.Crtime ?? 0);
+                    Console.WriteLine($"{file?.Name}\t({dateTime.ToString("dd.MM.yyyy HH:mm")})");
+                    var actualPath = file?.Path ?? string.Empty;
+                    var listResponse2 = await GetFileListAsync(client, synoToken, actualPath);
+                    if (listResponse2?.Success == true && listResponse2?.Data?.Files != null)
+                        foreach (var file2 in listResponse2.Data.Files)
+                        {
+                            Console.WriteLine($"\t{file2.Name}");
+                            actualPath = file2?.Path ?? string.Empty;
+                            var listResponse3 = await GetFileListAsync(client, synoToken, actualPath, "name", "asc", "file");
+                            if (listResponse3?.Success == true && listResponse3?.Data?.Files != null)
+                                foreach (var file3 in listResponse3.Data.Files)
+                                    Console.WriteLine($"\t\t{file3.Name}");
+                        }
+                }
+
+                Console.WriteLine("\nTotal files: " + listResponse.Data.Files.Count);
+            }
         }
     }
+
+    private static async Task<ApiAuthResponse> GetAuthAsync(string? account, string? passwd, HttpClient client)
+    {
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+        queryString["api"] = "SYNO.API.Auth";
+        queryString["version"] = "6";
+        queryString["method"] = "login";
+        queryString["account"] = account;
+        queryString["passwd"] = passwd;
+        queryString["enable_syno_token"] = "yes";
+        var url = string.Join("?", baseUrl, queryString.ToString());
+        var response = await client.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            string result = await response.Content.ReadAsStringAsync();
+            var authResponse = JsonSerializer.Deserialize<ApiAuthResponse>(result, jsonOptions);
+            if (authResponse?.Success == true)
+                return authResponse;
+        }
+
+        throw new Exception("Failed to authenticate");
+    }
+
+    private static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+    {
+        // Unix timestamp is seconds past epoch
+        DateTime dateTime = new(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+        return dateTime;
+    }
+
+    private static async Task<FileStationListResponse> GetFileListAsync(HttpClient client, string synoToken, string folderPath = "/music", string sortBy = "crtime", string sortDirection = "desc", string fileType = "dir")
+    {
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+        queryString["api"] = "SYNO.FileStation.List";
+        queryString["version"] = "2";
+        queryString["method"] = "list";
+        queryString["folder_path"] = folderPath;
+        queryString["sort_by"] = sortBy;
+        queryString["sort_direction"] = sortDirection;
+        queryString["filetype"] = fileType;
+        queryString["additional"] = "[\"owner\",\"time\"]";
+        queryString["SynoToken"] = synoToken;
+        var url = string.Join("?", baseUrl, queryString.ToString());
+        var response = await client.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsStringAsync();
+            var listResponse = JsonSerializer.Deserialize<FileStationListResponse>(result, jsonOptions);
+            if (listResponse?.Success == true && listResponse?.Data?.Files != null)
+                return listResponse;
+        }
+
+        throw new Exception($"Failed to read files from {folderPath}");
+    }
+
 }
 
 internal class ApiAuthResponse
@@ -91,4 +144,22 @@ internal class ListFile
 
 internal class Additional
 {
+    public Owner? Owner { get; set; }
+    public AdditionalTime? Time { get; set; }
+    public string? RealPath { get; set; }
+}
+
+internal class AdditionalTime
+{
+    public double? Crtime { get; set; }
+    public double? Ctime { get; set; }
+    public double? Mtime { get; set; }
+}
+
+internal class Owner
+{
+    public int? Gid { get; set; }
+    public string? Group { get; set; }
+    public int? Uid { get; set; }
+    public string? User { get; set; }
 }
